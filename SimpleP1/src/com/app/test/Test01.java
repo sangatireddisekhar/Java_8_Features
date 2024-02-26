427be157204160b41830694348eab094d27be136
HP <HP@DESKTOP-KJC7AI9>
2024-02-26 13:55:47 +0530

    2-commit-26/02/2024

---
+++ SimpleP1/src/com/app/test/Test01.java
@@ -0,0 +1,12 @@
+package com.app.test;
+
+public class Test01 {
+
+	public static void name() {
+		System.out.println("Hi");
+	}
+	public static void main(String[] args) {
+		System.out.println("Main method");
+		name();
+	}
+}
